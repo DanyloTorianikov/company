3c85f2f8f3f0244dafa967f754c671444cf2575b
DanyloTorianikov <danylo.torianikov.dev@gmail.com>
2023-03-07 16:45:59 +0200

    Added workspace styles

--- apps/frontend/crm/src/app/app.module.ts
+++ apps/frontend/crm/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
