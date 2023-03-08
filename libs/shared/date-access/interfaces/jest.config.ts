6ad1c3469abaae32b2e695778f08e9bfd4d52ba4
DanyloTorianikov <danylo.torianikov.dev@gmail.com>
2023-03-08 15:39:39 +0200

    Added data-access lib interfaces and tags for libs

---
+++ libs/shared/date-access/interfaces/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'shared-date-access-interfaces',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/shared/date-access/interfaces',
+};
